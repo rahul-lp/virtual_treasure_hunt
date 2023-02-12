0ef5279e99d3fff489d4d78fb11904de358419e4
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-02-12 18:08:34 +0530

    Created new riddles page

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,23 @@
-
-
-
 // execute the code when document is fully loaded
 $(document).ready(function () {
-    revealStory();
-
+  revealStory();
 });
 
-
-function revealStory(){    
+function revealStory() {
+  acn_btn_1 = document.getElementById("action_btn_1");
+  acn_btn_1.addEventListener("click", () => {
+    riddle = document.getElementById("riddle");
+    story = document.getElementById("story");
+    filter = document.getElementById("filter");
+    code_input = document.getElementById("code_input");
     acn_btn_1 = document.getElementById("action_btn_1");
-    acn_btn_1.addEventListener("click", () => {
+    acn_btn_2 = document.getElementById("action_btn_2");
 
-        riddle = document.getElementById("riddle");
-        story = document.getElementById("story");
-        filter = document.getElementById("filter");
-        code_input = document.getElementById("code_input");
-        acn_btn_1 = document.getElementById("action_btn_1");
-        acn_btn_2 = document.getElementById("action_btn_2");
-        
-        riddle.classList.add('d-none')
-        filter.classList.add('d-none')
-        code_input.classList.add('d-none')
-        acn_btn_1.classList.add('d-none')
-        acn_btn_2.classList.remove('d-none')
-        story.classList.remove('d-none')
-        
-    });
+    riddle.classList.add("d-none");
+    filter.classList.add("d-none");
+    code_input.classList.add("d-none");
+    acn_btn_1.classList.add("d-none");
+    acn_btn_2.classList.remove("d-none");
+    story.classList.remove("d-none");
+  });
 }
