ad10b1ce30855e91ca95a3b368e0a873f4cc9f9a
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-02-12 18:55:15 +0530

    Added JS for changing section

---
+++ javascript/riddles.js
@@ -0,0 +1,6 @@
+function revealStory() {
+  document.getElementById("r1section").style.display = "none";
+  document.getElementById("bottom1").style.display = "none";
+  document.getElementById("storysection1").style.display = "flex";
+  document.getElementById("nextriddlesection1").style.display = "flex";
+}
