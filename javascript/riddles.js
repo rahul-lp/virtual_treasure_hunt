98d26df87e965c37865d5c0c75f566e2312df459
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-02-13 00:26:37 +0530

    Riddle page frontend

--- javascript/riddles.js
+++ javascript/riddles.js
@@ -1,6 +1,32 @@
 function revealStory() {
+  document.getElementById("logo").style.display = "none";
   document.getElementById("r1section").style.display = "none";
   document.getElementById("bottom1").style.display = "none";
   document.getElementById("storysection1").style.display = "flex";
   document.getElementById("nextriddlesection1").style.display = "flex";
 }
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("instamodal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "flex";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
